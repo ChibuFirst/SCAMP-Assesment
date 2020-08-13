b4eb4745aca58dd9f37d8d96fa379b18623d522d
ChibuFirst <chibuzoonuoha57@gmail.com>
2020-08-13 22:36:09 +0100

    chore: updated the home page

--- app/src/main/java/com/chibufirst/scampassesment/MainActivity.kt
+++ app/src/main/java/com/chibufirst/scampassesment/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.chibufirst.scampassesment
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openStatsActivity(view: View) {}
+
+    fun createPhoneIntent(view: View) {
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:080097000010")
+        startActivity(intent)
+    }
+
+    fun createMessageIntent(view: View) {
+        val messageIntent = Intent(Intent.ACTION_SENDTO)
+        messageIntent.type = "vnd.android-dir/mms-sms"
+        messageIntent.data = Uri.parse("smsto" +
+                ":" + Uri.encode("080097000010"))
+        startActivity(messageIntent)
     }
 }
